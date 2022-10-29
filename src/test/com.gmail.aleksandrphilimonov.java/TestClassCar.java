57222ab082d5668f01978a4bee6e7639d379fbc1
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-10-29 21:36:42 +0300

    using parallel execution mode 'CONCURRENT'

--- src/test/com.gmail.aleksandrphilimonov.java/TestClassCar.java
+++ src/test/com.gmail.aleksandrphilimonov.java/TestClassCar.java
@@ -1,22 +1,30 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.DisabledOnJre;
-import org.junit.jupiter.api.condition.DisabledOnOs;
-import org.junit.jupiter.api.condition.EnabledOnJre;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-import org.junit.jupiter.api.condition.JRE;
-import org.junit.jupiter.api.condition.OS;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TestClassCar {
 
     @Test
-    @EnabledOnOs({OS.WINDOWS, OS.MAC})
-    @DisabledOnOs({OS.LINUX, OS.AIX})
-    @EnabledOnJre({JRE.JAVA_11, JRE.JAVA_9})
-    @DisabledOnJre({JRE.JAVA_12, JRE.JAVA_8})
+    public void getCarManufacturer() {
+        Car car = new Car("Audi", "7898-RTY", 2012, "Aleksandr Philimonov");
+        assertEquals("Audi", car.getManufacturer());
+    }
+
+    @Test
+    public void getCarNumber() {
+        Car car = new Car("Audi", "7898-RTY", 2012, "Aleksandr Philimonov");
+        assertEquals("7898-RTY", car.getNumber());
+    }
+
+    @Test
     public void getCarYear() {
         Car car = new Car("Audi", "7898-RTY", 2012, "Aleksandr Philimonov");
         assertEquals(2012, car.getYear());
+    }
+
+    @Test
+    public void getCarOwner() {
+        Car car = new Car("Audi", "7898-RTY", 2012, "Aleksandr Philimonov");
+        assertEquals("Aleksandr Philimonov", car.getOwner());
     }
 }
