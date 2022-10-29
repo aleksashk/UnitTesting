6f84a96bdb2c21e40f52b9530a3c1d5ec216e442
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2022-10-29 21:19:38 +0300

    @DisabledOnJre

--- src/test/com.gmail.aleksandrphilimonov.java/TestClassCar.java
+++ src/test/com.gmail.aleksandrphilimonov.java/TestClassCar.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 import org.junit.jupiter.api.condition.JRE;
@@ -11,6 +12,7 @@ public class TestClassCar {
     @Test
     @EnabledOnOs(OS.WINDOWS)
     @EnabledOnJre(JRE.JAVA_11)
+    @DisabledOnJre(JRE.JAVA_8)
     public void getCarYear() {
         Car car = new Car("Audi", "7898-RTY", 2012, "Aleksandr Philimonov");
         assertEquals(2012, car.getYear());
